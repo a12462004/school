3ea63b0f856adedd5d906cfeaae78262b02ef50b
a12462004 <a12462004@gmail.com>
2017-06-19 09:33:03 -0400

    all

---
+++ app/models/lost_item.rb
@@ -0,0 +1,15 @@
+class LostItem < ApplicationRecord
+  belongs_to :user
+  validates :title, :description, presence: true
+  
+  scope :available, -> {where(is_approved:true)}
+  
+  def self.available_for(user)
+    case user.role
+      when "staff"
+        all
+      else
+        available
+    end
+  end
+end
